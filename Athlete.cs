03701865ddd628bacdab908d5d771d96e4d4f876
Darrell Williams <dwilliams10@usf.edu>
2019-09-24 23:48:24 -0400

    Add project files.

---
+++ Athlete.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Athlete : Person
+    {
+        public string jerseyNumber;
+        public Person assistantCoach;
+
+
+
+        /// <summary>
+
+        /// Default constructor attempt 1
+
+        /// </summary>
+
+        public Athlete()
+
+        {
+
+            jerseyNumber = "22";
+
+            lastName = "Doe";
+
+        }
+
+
+
+        /// <summary>
+
+        /// Default constructor that uses inheritance
+
+        /// </summary>
+
+        //public Athlete() : base()
+
+        //{
+
+        //  jerseyNumber = "22";
+
+        //} 
+    }
+}
